b6bd09d1e4312bbc1fb0565216c3d9375792f90e
Júlia Dall'Agnol <juliadak@icloud.com>
2021-10-04 00:07:00 -0300

    checkpoint

--- app/Exports/TableExport.php
+++ app/Exports/TableExport.php
@@ -7,56 +7,70 @@ use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Illuminate\Support\Facades\Storage;
 
+
+function any(array $array, callable $fn) {
+    foreach ($array as $value) {
+        if($fn($value)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 class TableExport implements FromArray, ShouldAutoSize, WithHeadings
 {
+    public static function formatNumber($number) {
+        return floatval(str_replace([".", ","], ["", "."], $number));
+    }
+
+    public static function formatDateTime($dateTime) {
+        return date('Y-m-d h:i', strtotime(str_replace("/", "-", $dateTime)));
+    }
+
     public function array(): array {
         $logFile = fopen("logs.txt", "w");
+
         $contents = (string) Storage::get('FBAA_MASK.PRN');
-        fwrite($logFile, "Extração dos dados do arquivo PRN e conversão para string");
+        $lines = explode("\n", $contents);
 
-        $lines = array_filter(explode("\n", $contents), function($line) {
-            return $line !== "\r" && !str_contains($line, "Total");
-        });
-        fwrite($logFile, "\nTransformação do conteúdo do arquivo em um arranjo de linhas");
-        fwrite($logFile, "\nFiltração das linhas que não incluiam os dados a serem colocados na tabela");
-        
-        $res = array();
-
-        fwrite($logFile, "\nProcessamento das páginas:");
-        for ($i = 0; $i < 300; $i++) {
-            fwrite($logFile, "\nEliminação do cabeçalho da página {$i}");
-
-            $lines = array_splice($lines, 7);
-            $found = false;
-            fwrite($logFile, "\nEncontrar o index da linha de término da página");
-            for ($j = 0; $j < sizeof($lines) && !$found; $j++) {
-                if (str_contains($lines[$j], "---")) {
-                    fwrite($logFile, "\nRemoção dos dados da página de acordo com o index encontrado anteriormente");
-                    array_push($res, ...array_splice($lines, 0, $j));
-                    $found = true;
-                    fwrite($logFile, "\nAnálise da próxima página\n");
-                }
-            }
-        }
-
+        $lines = array_values(
+            array_filter(
+                $lines, 
+                fn($line) => !any(
+                    [
+                        fn($line) => str_contains($line, " ") && !str_contains($line, "-") && !str_contains($line, "/"),
+                        fn($line) => str_contains($line, "COOP CRED POUP E INV SICREDI PIONEIRA RS"),
+                        fn($line) => str_contains($line, "SISTEMA SICREDI - SISTEMA DE ATENDIMENTO - A.15"),
+                        fn($line) => str_contains($line, "MOVIMENTO DIARIO DO CONTA CORRENTE"),
+                        fn($line) => str_contains($line, "======"),
+                        fn($line) => str_contains($line, "------"),
+                        fn($line) => str_contains($line, "Origem  Conta   Nome Correntista"),
+                        fn($line) => str_contains($line, "Data/Hora"),
+                        fn($line) => str_contains($line, "Total"),
+                        fn($line) => str_contains($line, "TOTAIS DO DIA"),
+                        fn($line) => strlen($line) < 4
+                    ], 
+                    fn ($filter) => $filter($line)
+                )
+            )   
+        );
 
         $finalArr = array();
 
-        fwrite($logFile, "\nAnálise dos dados de cada página, tipagem e inserção no arranjo para geração da tabela");
-        for ($k = 0; $k <= count($res) - 1; $k = $k + 2) {
+        for ($k = 0; $k <= count($lines) - 2; $k = $k + 2) {
             array_push($finalArr, 
-            [
-                substr($res[$k], 0, 4), 
-                (int) substr($res[$k], 5, 2), 
-                (int) substr($res[$k], 8, 7), 
-                substr($res[$k], 16, 27), 
-                substr($res[$k], 48, 8), 
-                substr($res[$k], 58, 3), 
-                substr($res[$k], 62, 17),
-                floatval(str_replace([".", ","], ["", "."], substr($res[$k], 101, 9))),
-                floatval(str_replace([".", ","], ["", "."], substr($res[$k], 120, 9))),
-                date('Y-m-d', strtotime(str_replace("/", "-", substr($res[$k + 1], 116, 10))))
-            ]
+                [
+                    (int) substr($lines[$k], 0, 4), 
+                    (int) substr($lines[$k], 5, 2), 
+                    (int) substr($lines[$k], 8, 7), 
+                    substr($lines[$k], 16, 27), 
+                    substr($lines[$k], 48, 8), 
+                    substr($lines[$k], 58, 3), 
+                    substr($lines[$k], 62, 17),
+                    TableExport::formatNumber(substr($lines[$k], 101, 9)),
+                    TableExport::formatNumber(substr($lines[$k], 120, 9)),
+                    TableExport::formatDateTime($lines[$k + 1], 116, 16)
+                ]
             );
         }
 
